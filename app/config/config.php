fc3d5fd57ae5b51470f35048074ef3ba23500134
Shivaji Chalise <respond2shivaji@gmail.com>
2022-06-23 16:36:49 +0545

    Defined some constants and added autoloader for core libraries

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+// App root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// URL root
+define('URLROOT', 'http://localhost/projects/php/samsara');
+
+// Site name
+define('SITENAME', 'Samsara');
