7e61f423d3f74bcaa5ae476e5c0a4f0fa9f2976e
Shivaji Chalise <respond2shivaji@gmail.com>
2022-06-27 13:13:40 +0545

    Added LoggedIn user session data and logout method

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -135,7 +135,7 @@ class Users extends Controller
         $loggedInUser = $this->userModel->login($data['email'], $data['password']);
         if ($loggedInUser) {
           // Create Session
-          die('Success');
+          $this->createUserSession($loggedInUser);
         } else {
           $data['password_error'] = 'Incorrect Password';
           $this->view('users/login', $data);
@@ -154,6 +154,32 @@ class Users extends Controller
 
       // Load view
       $this->view('users/login', $data);
+    }
+  }
+
+  public function createUserSession($user)
+  {
+    $_SESSION['user_id'] = $user->id;
+    $_SESSION['user_name'] = $user->name;
+    $_SESSION['user_email'] = $user->email;
+    redirect('pages/index');
+  }
+
+  public function logout()
+  {
+    unset($_SESSION['user_id']);
+    unset($_SESSION['user_name']);
+    unset($_SESSION['user_email']);
+    session_destroy();
+    redirect('users/login');
+  }
+
+  public function isLoggedIn()
+  {
+    if (isset($_SESSION['user_id'])) {
+      return true;
+    } else {
+      return false;
     }
   }
 }
