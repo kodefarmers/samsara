2c40bdf0111354f40474d5e266210f3e22b58447
Shivaji Chalise <respond2shivaji@gmail.com>
2022-06-29 21:36:23 +0545

    Added Popup Modal (Half Done)

--- app/views/includes/main.php
+++ app/views/includes/main.php
@@ -16,11 +16,39 @@
         </a>
       </div>
     </div>
-    <!--    
     <div class="main__container-right">
       <div class="contents">
-      </div> 
-    </div> 
-      -->
+        <!-- Clock -->
+        <div class="child contents-child1">Child1</div>
+        <!-- Weather -->
+        <div class="child contents-child2">Child2</div>
+        <!-- TODO -->
+        <div class="child contents-child3">
+          <div class="child3-head">
+            <span>todo</span><button><i class="uil uil-plus-square"></i></button>
+          </div>
+          <div class="child3-content">
+            <div class="child3-content-item1">
+              <a>
+                <span><i class="uil uil-check-circle"></i></span>
+              </a> do dinner
+              <a>
+                <span><i class="uil uil-edit"></i></span>
+              </a>
+              <a>
+                <span><i class="uil uil-trash-alt"></i></span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <!-- Notes -->
+        <div class="child contents-child4">Child4</div>
+        <!-- Hourly Weather -->
+        <div class="child contents-child5">Child5</div>
+      </div>
+    </div>
   </div>
 </main>
+<?php
+require_once 'popup-modal.php';
+?>
---
+++ app/views/includes/popup-modal.php
@@ -0,0 +1,20 @@
+<div class="popup">
+  <div class="popup-card">
+    <h2>Add Todo</h2>
+    <p>Give title, body and set remainder!</p>
+    <div class="popup-card-title">
+      <h4>Title</h4>
+      <input type="text" name="title" id="" placeholder="Title" />
+    </div>
+    <div class="popup-card-body">
+      <h4>Description</h4>
+      <textarea name="" id="" cols="30" rows="10"></textarea>
+    </div>
+    <div class="popup-card-options">
+      <a>
+        <span><i class="uil uil-stopwatch"></i></span>
+        <input type="submit" value="Save">
+      </a>
+    </div>
+  </div>
+</div>
