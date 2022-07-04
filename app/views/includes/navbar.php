726d34f0a872b5f0fafa490d76bf511447210014
Shivaji Chalise <respond2shivaji@gmail.com>
2022-07-04 16:11:57 +0545

    Added Navbar dropdown for logout

--- app/views/includes/navbar.php
+++ app/views/includes/navbar.php
@@ -7,6 +7,14 @@
       <div class="nav__profile-image">
         <img src="<?php echo URLROOT; ?>/img/avatar.png" alt="avatar" />
       </div>
+      <div class="nav__profile-dropdown">
+        <a class="dropdown">
+          <span><i class="uil uil-setting"></i></span>
+        </a>
+        <div class="dropdown-contents">
+          <a href="<?php echo URLROOT ?>/users/logout">Logout</a>
+        </div>
+      </div>
     </div>
   </div>
 </nav>
