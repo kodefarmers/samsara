6737848302a7ac46d6fcb2c8c3640381eda8e4ef
Yakeen Kapali <75461426+yakeenkapali@users.noreply.github.com>
2022-07-08 14:06:51 +0545

    Including datetime.js

--- app/views/includes/footer.php
+++ app/views/includes/footer.php
@@ -1,6 +1,7 @@
 <script src="<?php echo URLROOT; ?>/js/main.js"></script>
 <script src="<?php echo URLROOT; ?>/js/popup.js"></script>
 <script src="<?php echo URLROOT; ?>/js/checkTodo.js"></script>
+<script src="<?php echo URLROOT; ?>/js/datetime.js"></script>
 
 </body>
 
