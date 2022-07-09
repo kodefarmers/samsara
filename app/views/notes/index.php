176774774ec6b0af8f94cd2982df5ec39b1ded83
Yakeen Kapali <75461426+yakeenkapali@users.noreply.github.com>
2022-07-09 13:55:43 +0545

    Adding Note Page html

--- app/views/notes/index.php
+++ app/views/notes/index.php
@@ -2,6 +2,35 @@
 <?php require APPROOT . '/views/includes/navbar.php'; ?>
 <?php require APPROOT . '/views/includes/mainh.php'; ?>
 
+<div class="notes">
+  <?php foreach ($data['notes'] as $note) : ?>
+    <div class="note">
+      <div class="note-title">
+        <h4> <?php echo (strlen($note->title) > 26) ? substr($note->title, 0, 26) . '...' : $note->title ?> </h4>
+        <span><?php echo date("M d, Y", strtotime($note->updated_at));  ?></span>
+      </div>
+      <div class="note-description">
+        <p>
+          <?php echo (strlen($note->description) > 220) ? substr($note->description, 0, 219) . '...' : $note->description  ?>
+        </p>
+      </div>
+      <div class="note-options">
+        <button class="btn btn-primary view-note">
+          <i id="<?php echo $note->noteId ?>" class="uil uil-eye view-note"></i>
+        </button>
+        </button>
+        <button class="edit btn btn-success edit-note">
+          <i id="<?php echo $note->noteId ?>" class="uil uil-edit edit-note"></i>
+        </button>
+        <form action="<?php echo URLROOT; ?>/notes/delete/<?php echo $note->noteId ?>" method="POST">
+          <button class="btn btn-danger">
+            <i class="uil uil-trash-alt"></i>
+          </button>
+        </form>
+      </div>
+    </div>
+  <?php endforeach; ?>
+</div>
 
 <?php require APPROOT . '/views/includes/mainf.php'; ?>
 <?php require APPROOT . '/views/includes/popup-modal.php'; ?>
