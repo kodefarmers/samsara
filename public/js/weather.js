dc205b126aa9d226c6830ed4c8e476c879a02faa
Yakeen Kapali <75461426+yakeenkapali@users.noreply.github.com>
2022-07-09 12:24:33 +0545

    Show only time

--- public/js/weather.js
+++ public/js/weather.js
@@ -14,12 +14,43 @@ let weather = {
     const {icon, description} = data.weather[0];
     const {temp, humidity} = data.main;
     const {sunrise, sunset} = data.sys;
+        let sr = new Date(sunrise * 1000);
+    let ss = new Date(sunset * 1000);
+
+    let sr_hours = sr.getHours();
+    let sr_minutes = sr.getMinutes();
+    let sr_seconds = sr.getSeconds();
+
+    if (+sr_hours <= 9) {
+      sr_hours = `0${sr_hours}`;
+    }
+    if (+sr_minutes <= 9) {
+      sr_minutes = `0${sr_minutes}`;
+    }
+    if (+sr_seconds <= 9) {
+      sr_seconds = `0${sr_seconds}`;
+    }
+
+    let ss_hours = ss.getHours();
+    let ss_minutes = ss.getMinutes();
+    let ss_seconds = ss.getSeconds();
+
+    if (+ss_hours <= 9) {
+      ss_hours = `0${ss_hours}`;
+    }
+    if (+ss_minutes <= 9) {
+      ss_minutes = `0${ss_minutes}`;
+    }
+    if (+ss_seconds <= 9) {
+      ss_seconds = `0${ss_seconds}`;
+    }
+
     document.querySelector(".weather-icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
     document.querySelector("#span-weather").innerText = description;
     document.querySelector("#span-temperature").innerText = temp + "°C";
     document.querySelector("#span-humidity").innerText = humidity + "%";
-    document.querySelector("#span-sunrise").innerText = new Date(sunrise*1000);
-    document.querySelector("#span-sunset").innerText = new Date(sunset*1000);
+    document.querySelector("#span-sunrise").innerText = `${sr_hours}:${sr_minutes}:${sr_seconds}`;
+    document.querySelector("#span-sunset").innerText = `${ss_hours}:${ss_minutes}:${ss_seconds}`;
   },
   search: function () {
     this.fetchWeather(document.querySelector('.city').value);
